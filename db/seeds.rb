908fe2d7ddaa3faab0df5f0127e9f532e4b7a1ad
Ryan Strawn <str.designanddev@gmail.com>
2018-10-09 09:38:06 -0600

    5.7 top and bottom comments fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Comment.create!(user: User.first, product: Product.first, rating: 1, body: "bad")
-Comment.create!(user: User.first, product: Product.first, rating: 4, body: "nice")
-Comment.create!(user: User.first, product: Product.first, rating: 2, body: "meh")
+Comment.create!(user: User.first, product: Product.first, rating: 1, body: "God I hate this bird. It doesn't even do any tricks.")
+Comment.create!(user: User.first, product: Product.first, rating: 3, body: "Ok bird I guess, I'd recommend but not with any enthusiasm. I mean, it is just a bird.")
+Comment.create!(user: User.first, product: Product.first, rating: 5, body: "This bird is my only true friend. I'd like to marry this bird.")
+
+Comment.create!(user: User.first, product: Product.second, rating: 1, body: "God I hate this bird. It doesn't even do any tricks.")
+Comment.create!(user: User.first, product: Product.second, rating: 3, body: "Ok bird I guess, I'd recommend but not with any enthusiasm. I mean, it is just a bird.")
+Comment.create!(user: User.first, product: Product.second, rating: 5, body: "This bird is my only true friend. I'd like to marry this bird.")
+
+Comment.create!(user: User.first, product: Product.last, rating: 1, body: "God I hate this bird. It doesn't even do any tricks.")
+Comment.create!(user: User.first, product: Product.last, rating: 3, body: "Ok bird I guess, I'd recommend but not with any enthusiasm. I mean, it is just a bird.")
+Comment.create!(user: User.first, product: Product.last, rating: 5, body: "This bird is my only true friend. I'd like to marry this bird.")
