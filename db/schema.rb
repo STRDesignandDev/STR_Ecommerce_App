e95d52a1cb03064c3b116f4a26de5aee4b267528
Ryan Strawn <str.designanddev@gmail.com>
2018-10-11 20:08:13 -0600

    ughh

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_003127) do
+ActiveRecord::Schema.define(version: 2018_10_12_004512) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2018_10_12_003127) do
     t.string "color"
     t.integer "price"
     t.string "birdtype"
-    t.string "type"
   end
 
   create_table "users", force: :cascade do |t|
