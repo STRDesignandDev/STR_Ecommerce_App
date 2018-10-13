6f7d3eec86ac85203c1d1457a9576144852b2ade
Ryan Strawn <str.designanddev@gmail.com>
2018-10-12 18:39:36 -0600

    ___

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -79,6 +79,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url, :price)
+      params.require(:product).permit(:name, :description, :image_url, :price, :birdtype)
     end
 end
