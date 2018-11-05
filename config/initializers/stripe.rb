7df128db22d38356ffc184a25d0523c5b4b61a19
Ryan Strawn <str.designanddev@gmail.com>
2018-11-05 16:54:50 -0700

    trying to fix stripe still

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: ENV['pk_test_x2BbQOls7hIBkOB78fYjPXL0'],
-    secret_key: ENV['sk_test_vfqSySjrcl7ltJZf9L5Glax8']
+    publishable_key: 'pk_test_x2BbQOls7hIBkOB78fYjPXL0',
+    secret_key: 'sk_test_vfqSySjrcl7ltJZf9L5Glax8'
   }
 else
   Rails.configuration.stripe = {
