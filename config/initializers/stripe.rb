7d9077a4e20db4b31bc038ad53e6ab2d00712efb
Ryan Strawn <str.designanddev@gmail.com>
2018-11-05 17:02:59 -0700

    stripe is a bummer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
-    publishable_key: 'pk_test_x2BbQOls7hIBkOB78fYjPXL0',
-    secret_key: 'sk_test_vfqSySjrcl7ltJZf9L5Glax8'
+    publishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+    secret_key: ENV['STRIPE_SECRET_KEY']
   }
 else
   Rails.configuration.stripe = {
