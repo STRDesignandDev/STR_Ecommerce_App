fbd28711eea0fab61d075ce0a149579d503ab86c
Ryan Strawn <str.designanddev@gmail.com>
2018-12-23 12:11:13 -0700

    test

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
+  # before_action :authenticate_user!
+  # load_and_authorize_resource
 
 
     def create
